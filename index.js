0ae4dea09f67b718bc9ea61ef22b93c5260d982a
wikteska-ai <wikteska@gmail.com>
2026-03-25 15:38:28 +0100

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import http from "http";
-import { generateSchedule } from "./solver.js";
+import { queue } from "./queue.js";
 import fs from "fs";
 
 const PORT = process.env.PORT || 3000;
 
-let jobs = {}; // pamięć jobów
 
 function randomId() {
   return Math.random().toString(36).substr(2, 9);
@@ -47,33 +46,10 @@ const server = http.createServer(async (req, res) => {
         };
 
         // 🔥 ASYNC JOB
-        (async () => {
-          const start = Date.now();
-
-          try {
-
-            const result = await generateSchedule(data);
-
-            jobs[jobId] = {
-              status: "done",
-              result,
-              progress: {
-                percent: 100,
-                bestPlaced: result.placed || 0,
-                total: result.total || 0,
-                elapsed: Math.floor((Date.now() - start) / 1000)
-              }
-            };
-
-            console.log(`✅ DONE JOB ${jobId}`);
-
-          } catch (e) {
-            console.error(`❌ JOB ERROR ${jobId}`, e);
-
-            jobs[jobId] = { status: "fail" };
-          }
-
-        })();
+      await queue.add("generate", {
+  jobId,
+  data
+});
 
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ jobId }));
@@ -90,19 +66,37 @@ const server = http.createServer(async (req, res) => {
   // ===== STATUS =====
   if (req.url.startsWith("/status/") && req.method === "GET") {
 
-    const jobId = req.url.split("/")[2];
+  const jobId = req.url.split("/")[2];
 
-    if (!jobs[jobId]) {
-      res.writeHead(200);
-      res.end(JSON.stringify({ status: "not_found" }));
-      return;
-    }
+  const job = await queue.getJob(jobId);
 
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(jobs[jobId]));
+  if (!job) {
+    res.writeHead(200);
+    res.end(JSON.stringify({ status: "not_found" }));
     return;
   }
 
+  const state = await job.getState();
+
+  if (state === "completed") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({
+      status: "done",
+      result: job.returnvalue
+    }));
+    return;
+  }
+
+  if (state === "failed") {
+    res.writeHead(200);
+    res.end(JSON.stringify({ status: "fail" }));
+    return;
+  }
+
+  res.writeHead(200);
+  res.end(JSON.stringify({ status: state }));
+  return;
+}
   // ===== KEEP ALIVE =====
   if (req.url === "/" && req.method === "GET") {
     res.writeHead(200);
